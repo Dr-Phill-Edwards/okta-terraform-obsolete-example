b7fc1fcda963faec7e9a968e9104b5c0183f45ee
Dr Phill Edwards <phill@aux-quatre-vents-france.fr>
2021-01-12 12:21:49 +0100

    Built API server

---
+++ authorize/go.mod
@@ -0,0 +1,9 @@
+module authorize
+
+go 1.15
+
+require (
+	cloud.google.com/go/firestore v1.4.0
+	github.com/gin-gonic/gin v1.6.3
+	google.golang.org/api v0.36.0
+)
---
+++ authorize/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"cloud.google.com/go/firestore"
+	"github.com/gin-gonic/gin"
+	"google.golang.org/api/iterator"
+)
+
+var client *firestore.Client
+
+func GetUsers(c *gin.Context) {
+	var result []map[string]interface{}
+	iter := client.Collection("user-roles").Documents(context.Background())
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		result = append(result, doc.Data())
+	}
+	c.JSON(http.StatusOK, result)
+}
+
+func PostUser(c *gin.Context) {
+	status := http.StatusCreated
+	email := c.PostForm("email")
+	response := email
+	role := c.PostForm("role")
+	_, _, err := client.Collection("user-roles").Add(context.Background(), map[string]interface{}{"email": email, "role": role})
+	if err != nil {
+		status = http.StatusBadRequest
+		response = err.Error()
+	}
+	c.String(status, response)
+}
+
+func main() {
+	var err error
+	client, err = firestore.NewClient(context.Background(), os.Getenv("GOOGLE_PROJECT_ID"))
+	if err != nil {
+		fmt.Println("Error " + err.Error())
+	}
+	r := gin.Default()
+	r.GET("/api/authorize", GetUsers)
+	r.POST("/api/authorize", PostUser)
+	r.Run(":8080")
+}
